c670b4d9f8265eb09d7b237fdeaef00a390d7b22
freek boutkan <freek.boutkan@gmail.com>
2012-01-13 16:31:21 +0100

    d

--- htdocs/keuren.php
+++ htdocs/keuren.php
@@ -1,7 +1,7 @@
 <h2>Te keuren elementen</h2>
 <ul>
     <li>
-	<a href="index.php?page=evenement">Test-evenement 1</a>
+	<a href="index.php?page=evenement&amp;semipage=keuren">Test-evenement 1</a>
     </li>
     <li>
 	Test-evenement 2
