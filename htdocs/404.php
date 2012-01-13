26f6a640dfdd1bbfdf2f06a762ebd9b41a5572cc
David Woudenberg <david_woudenberg@hotmail.com>
2012-01-13 15:57:33 +0100

    html vervangen door php

---
+++ htdocs/404.php
@@ -0,0 +1,3 @@
+<h2>Pagina niet gevonden</h2>
+<p>
+	Deze pagina kan helaas nu niet gevonden worden. U kunt het later opnieuw proberen. Excuses</p>
---
+++ htdocs/agenda_week.php
@@ -0,0 +1,86 @@
+
+<form id="doelgroepen" action="">
+	<fieldset>
+	<legend>Categorieën</legend>
+	<label for="doelgroep_klant"><input type="checkbox" name="doelgroep" id="doelgroep_klant" value="Klant"  checked="checked" />Klant</label>
+	<label for="doelgroep_kok"><input type="checkbox" name="doelgroep" id="doelgroep_kok" value="Kok" checked="checked" />Keuken</label>
+	<label for="doelgroep_afwas"><input type="checkbox" name="doelgroep" id="doelgroep_afwas" value="Afwas" checked="checked" />Afwassers</label>
+	<label for="doelgroep_bar"><input type="checkbox" name="doelgroep" id="doelgroep_bar" value="Bar" checked="checked" />Barpersoneel</label>
+	</fieldset>
+</form>
+
+<a href="index.php?page=agenda_week" title="Vorige week">
+	<button>Vorige week</button>
+</a>
+
+<a href="index.php?page=agenda_week" title="Volgende week">
+	<button>Volgende week</button>
+</a>
+
+<div id="weeklijst">
+
+<h1>Maandag 9 januari 2012</h1>
+<ul class="leeg">   
+    <li>Geen evenementen</li>
+</ul>
+
+<h1>Dinsdag 10 januari 2012</h1>
+<ul>   
+    <li class="kok"><a href="index.php?page=evenement">Test evenement kok (10:00-17:00)</a></li>
+    <li class="afwas"><a href="index.php?page=evenement">Test afwas</a></li>
+</ul>
+
+<h1>Woensdag 11 januari 2012</h1>
+<ul class="leeg">  
+    <li>Geen evenementen</li>
+</ul>
+
+<h1>Donderdag 12 januari 2012</h1>
+<ul>     
+    <li class="bar"><a href="index.php?page=evenement">Test bar</a></li>
+    <li class="klant"><a href="index.php?page=evenement">Test klant</a></li>
+</ul>
+
+<h1>Vrijdag 13 januari 2012</h1>
+<ul class="leeg">     
+    <li>Geen evenementen</li>
+</ul>
+
+<h1>Zaterdag 14 januari 2012</h1>
+<ul class="leeg">    
+    <li>Geen evenementen</li>
+</ul>
+
+<h1>Zondag 15 januari 2012</h1>
+<ul class="leeg">    
+    <li>Geen evenementen</li>
+</ul>
+</div>
+
+<table border="1" width="100%" cellspacing="0">
+	<tr><th width="40px"></th><th width="14.2%">maandag</th><th width="14.2%">dinsdag</th><th width="14.2%">woensdag</th><th width="14.2%">donderdag</th><th width="14.2%">vrijdag</th><th width="14.2%">zaterdag</th><th width="14.2%">zondag</th></tr>
+	<tr id="u0"><td>00:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u1"><td>01:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u2"><td>02:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u3"><td>03:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u4"><td>04:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u5"><td>05:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u6"><td>06:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u7"><td>07:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u8"><td>08:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u9"><td>09:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u10"><td>10:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u11"><td>11:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u12"><td>12:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u13"><td>13:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u14"><td>14:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u15"><td>15:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u16"><td>16:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u17"><td>17:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u18"><td>18:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u19"><td>19:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u20"><td>20:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u21"><td>21:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u22"><td>22:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u23"><td>23:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+</table>
