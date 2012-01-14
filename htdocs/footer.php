803745387cb1de0319b2f69a9c82129da74ae16b
Ivan Plantevin <ivan.p92@gmail.com>
2012-01-14 16:36:10 +0100

    footer fix

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -1,10 +1,12 @@
 <div class="clearer">&nbsp;</div>
 </div>
 <div id="namen">
-&copy; 2012 Freek Boutkan
-Ivan Plantevin
-Vincent Velthuis
+<div id="namen_text">
+&copy; 2012 Freek Boutkan,
+Ivan Plantevin,
+Vincent Velthuis &amp;
 David Woudenberg
+</div>
 </div>
 </div>
 </body>
