62310b5ae6898e02f08f3fc6c63df63c2bd76ea5
Ivan Plantevin <ivan.p92@gmail.com>
2012-01-16 21:19:30 +0100

    404.php

--- htdocs/404.php
+++ htdocs/404.php
@@ -1,4 +1,5 @@
+<!-- Deze pagina wordt automatisch geladen wanneer iemand een verkeerd adres intypt -->
+
 <h2>Pagina niet gevonden</h2>
-<p>
-	HAHAHABLABLABLA
-	Deze pagina kan helaas nu niet gevonden worden. U kunt het later opnieuw proberen. Excuses</p>
+
+<p>Deze pagina kan helaas nu niet gevonden worden. U kunt het later opnieuw proberen. Excuses</p>
