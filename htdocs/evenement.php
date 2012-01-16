22f5e75c3d398126ab23d6afc12cf5bf2aa1d9d6
Vincent Velthuis <v.velthuis@gmail.com>
2012-01-16 14:31:52 +0100

    tabel evenement

--- htdocs/evenement.php
+++ htdocs/evenement.php
@@ -1,13 +1,53 @@
-<table>
-<tr><td>Titel</td><td>Hier komt de titel</td></tr>
-<tr><td>Omschrijving</td><td>Maar deze tabel moet mooier gemaakt worden</td></tr>
-<tr><td>Begintijd</td><td>Hier komt de begintijd en datum</td></tr>
-<tr><td>Eindtijd</td><td>Hier de eindtijd en datum</td></tr>
-<tr><td>Klant</td><td>Hier een checkplaatje, of niets</td></tr>
-<tr><td>Keuken</td><td>Hier een checkplaatje, of niets</td></tr>
-<tr><td>Afwassers</td><td>Hier een checkplaatje, of niets</td></tr>
-<tr><td>Barpersoneel</td><td>Hier een checkplaatje, of niets</td></tr>
+<table id="evenement">
+	<tbody>
+	<tr>
+		<td>Titel</td>
+		<td class="rechts">Hier komt de titel</td>
+	</tr>
+	<tr>
+		<td>Plaatsing</td>
+		<td class="rechts">Datum van toevoegen en auteur</td>
+	</tr>
+	<tr>
+		<td>Goedkeuring</td>
+		<td class="rechts">Datum van goedkeuren en autorisator</td>
+	</tr>
+	<tr>
+		<td>Omschrijving</td>
+		<td class="rechts">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+			Mauris vel magna. Mauris risus nunc, tristique varius, gravida in, 
+			lacinia vel, elit.Nam ornare, felis non faucibus molestie, 
+			nulla augue adipiscing mauris, a nonummy diam ligula ut risus. 
+			Praesent varius. Cum sociis natoque penatibus et magnis dis parturient montes, 
+			nascetur ridiculus mus.</td>
+	</tr>
+	<tr>
+		<td>Locatie</td>
+		<td>Hier komt de locatie</td>
+	<tr>
+		<td>Begintijd</td>
+		<td class="rechts">Hier komt de begintijd en datum</td>
+	</tr>
+	<tr>
+		<td>Eindtijd</td>
+		<td class="rechts">Hier de eindtijd en datum</td>
+	</tr>
+	<tr>
+		<td rowspan="4">Categorie</td>
+		<td class="rechts">(check) Klant</td>
+	</tr>
+	<tr>
+		<td class="rechts">(check) Keuken</td>
+	</tr>
+	<tr>
+		<td class="rechts">(check) Afwassers</td>
+	</tr>
+	<tr>
+		<td class="rechts">(check) Barpersoneel</td>
+	</tr>
+	</tbody>
 </table>
+
 <a class="button" href="#"onclick="this.blur();"><span>Goedkeuren</span></a>
 <a class="button" href="#"onclick="this.blur();"><span>Afkeuren</span></a>
 <a class="button" href="#"onclick="this.blur();"><span>Aanpassen</span></a>
