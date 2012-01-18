5ec3d0b10c1fd588256063db5a97eea10e2cf2eb
David Woudenberg <david_woudenberg@hotmail.com>
2012-01-18 13:39:12 +0100

    sorry

--- htdocs/header.php
+++ htdocs/header.php
@@ -43,7 +43,7 @@
 			}
 			
 			$menuItems = array('agenda_week' => 'Agenda', 'toevoeg_evenement' => 'Evenement toevoegen', 
-				'keuren' => 'Evenement goedkeuren' 'lijst_van_gebruikers' => 'Gebruikers rechten aanpassen');
+				'keuren' => 'Evenement goedkeuren', 'lijst_van_gebruikers' => 'Gebruikers rechten aanpassen');
 			
 			foreach($menuItems as $fileName => $screenName)
 			{
