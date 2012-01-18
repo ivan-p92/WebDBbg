0e544212481a3a70e086c85ec5706706ea60d662
Ivan Plantevin <ivan.p92@gmail.com>
2012-01-18 13:36:15 +0100

    keuren wijziging met gelijksoortige lijstweergave als bij agenda pagina

--- htdocs/keuren.php
+++ htdocs/keuren.php
@@ -1,9 +1,36 @@
-<h2>Te keuren elementen</h2>
-<ul>
-    <li>
-	<a href="index.php?page=evenement&amp;semipage=keuren">Test-evenement 1</a>
-    </li>
-    <li>
-	Test-evenement 2
-    </li>
-</ul>
+<div id="event_lijst_container">
+        <h1 id="event_lijst_titel">Te keuren evenementen</h1>
+        <ul class="event_lijst">
+
+                <!-- Elk list item is een evenement -->
+                <li class="event">
+                        <p class="eendags_event">
+                                <span class="begin_datum">
+                                        <span class="jaar">2012</span>
+                                        <span class="dd-mm">16<br />JAN</span>
+                                </span>
+                        </p>
+                        <div class="event_details">
+                                <p class="event_titel">
+                                        <a class="event_link" href="index.php?page=evenement&amp;semipage=agenda_week">Titel van het event</a>
+                                </p>
+                                <p class="begintijd">11:00u - 19:00u</p>
+                        </div>
+                </li>
+
+                <li class="event">
+                        <p class="eendags_event">
+                                <span class="begin_datum">
+                                        <span class="jaar">2012</span>
+                                        <span class="dd-mm">18 JAN<br />19 JAN</span>
+                                </span>
+                        </p>
+                        <div class="event_details">
+                                <p class="event_titel">
+                                        <a class="event_link" href="index.php?page=evenement&amp;semipage=agenda_week">Titel van het event</a>
+                                </p>
+                                <p class="begintijd">12:00u - 3:00u</p>
+                        </div>
+                </li>
+        </ul>
+</div>
