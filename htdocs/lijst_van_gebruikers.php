9179b763ba4a24fea7cdab8a7190606cb349fbc8
David Woudenberg <david_woudenberg@hotmail.com>
2012-01-18 13:41:04 +0100

    laatste bug er ook uit

--- htdocs/lijst_van_gebruikers.php
+++ htdocs/lijst_van_gebruikers.php
@@ -3,16 +3,16 @@
 	<p>Klik op een gebruiker om gegevens van deze gebruiker te bekijken en zijn rechten aan te passen
 		<ul>
 		<li>
-		<a href="index.php?page=admin&amp;semipage=admin">Freek Boutkan</a>
+		<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">Freek Boutkan</a>
 		</li>
 		<li>
-		<a href="index.php?page=admin&amp;semipage=admin">Ivan Plantevin</a>
+		<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">Ivan Plantevin</a>
 		</li>
 		<li>
-		<a href="index.php?page=admin&amp;semipage=admin">Vincent Velthuis</a>
+		<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">Vincent Velthuis</a>
 		</li>
 		<li>
-		<a href="index.php?page=admin&amp;semipage=admin">David Woudenberg</a>
+		<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">David Woudenberg</a>
 		</li>
 		</ul>
 	</p>	
