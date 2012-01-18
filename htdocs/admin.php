31a7075ea5b902be7b9f262d0a29166d46bd1103
David Woudenberg <david_woudenberg@hotmail.com>
2012-01-18 13:18:17 +0100

    admin pagina toegevoegd

---
+++ htdocs/admin.php
@@ -0,0 +1,18 @@
+<div class="admin>
+<h1>Pas de rechten van %naam% aan</h1>
+<p>Op dit moment heeft %naam% de volgende rechten:
+<ul>
+<li>
+geen
+</li>
+</ul>
+</p>
+<p>
+Geef %naam% de volgende rechten:
+<form id="admin" action="" method="get">
+<input type="checkbox" value="create">Evenementen aanmaken
+<input type="checkbox" value="approve">Evenementen keuren
+<input type="checkbox" value="admin">Admin rechten
+</form>
+</p>
+</div>
