70c7c20464ef39f8cf39a292739a1a8bccfc5652
Ivan Plantevin <ivan.p92@gmail.com>
2012-01-18 13:24:34 +0100

    account info pagina

---
+++ htdocs/account.php
@@ -0,0 +1,32 @@
+<h1>Account informatie</h1>
+
+<p> Op deze pagina vindt u informatie over uw account. <br />
+    U kunt in het onderstaande formulier ook uw wachtwoord wijzigen.</p>
+
+<table>
+<tbody>
+	<tr>
+	<td>Accountnaam (email)</td><td>Het betreffende email-adres</td>
+	</tr>
+	<td>Huidige permissies</td><td>Evenementen toevoegen/goedkeuren/admin</td>
+</tbody>
+</table>
+
+<form id="wijzigww" action="" method="get">
+<table>
+<tbody>
+	<tr>
+	<td>Oud wachtwoord: </td><td><input type="password" id="oldpswd" name="mail" /></td>
+	</tr>
+	<tr>
+	<td>Nieuw wachtwoord: </td><td><input type="password" id="newpswd" name="pswd" /></td>
+	</tr>
+	<tr>
+	<td>Wachtwoord nogmaals: </td><td><input type="password" id="newpswd2" name="pswd2"></td>
+	</tr>
+	<tr>
+	<td>&nbsp;</td><td><input type="submit" value="Registreer" /><input type="reset" value="Velden wissen" /></td>
+	</tr>
+</tbody>
+</table>
+</form>
