dcefdf29d3e17c7cbbc8afece6b1bfb54d3f60bd
freek boutkan <freek.boutkan@gmail.com>
2012-01-18 16:05:37 +0100

    Modi

--- htdocs/agenda_week.php
+++ htdocs/agenda_week.php
@@ -1,86 +1,22 @@
 <!-- Header wordt hiervoor geladen -->
-
-<!-- Deze form is voor het selecteren van de te tonen doelgroepen -->
-<form id="doelgroepen" action="">
-	<fieldset>
-	<legend>Categorie&euml;n</legend>
-	<label for="doelgroep_klant"><input type="checkbox" name="doelgroep" id="doelgroep_klant" value="Klant"  checked="checked" />Klant</label><br />
-	<label for="doelgroep_kok"><input type="checkbox" name="doelgroep" id="doelgroep_kok" value="Kok" checked="checked" />Keuken</label><br />
-	<label for="doelgroep_afwas"><input type="checkbox" name="doelgroep" id="doelgroep_afwas" value="Afwas" checked="checked" />Afwassers</label><br />
-	<label for="doelgroep_bar"><input type="checkbox" name="doelgroep" id="doelgroep_bar" value="Bar" checked="checked" />Barpersoneel</label><br />
-	</fieldset>
-</form>
-
-<!-- div#weeklijst container bevat de lijst van de evenementen 
-     De hoofdstructuur ervan is een unordered list -->
-<div id="event_lijst_container">
-	<h1 id="event_lijst_titel">Aankomende evenementen</h1>
-	<ul class="event_lijst">
-
-		<!-- Elk list item is een evenement -->
-		<li class="event">
-			<p class="eendags_event">
-				<span class="begin_datum">
-					<span class="jaar">2012</span>
-					<span class="dd-mm">16<br />JAN</span>
-				</span>
-			</p>
-			<div class="event_details">
-				<p class="event_titel">
-					<a class="event_link" href="index.php?page=evenement&amp;semipage=agenda_week">Titel van het event</a>
-				</p>
-				<p class="begintijd">11:00u - 19:00u</p>
-			</div>
-		</li>
-
-		<li class="event">
-			<p class="eendags_event">
-				<span class="begin_datum">
-					<span class="jaar">2012</span>
-					<span class="dd-mm">18 JAN<br />19 JAN</span>
-				</span>
-			</p>
-			<div class="event_details">
-				<p class="event_titel">
-					<a class="event_link" href="index.php?page=evenement&amp;semipage=agenda_week">Titel van het event</a>
-				</p>
-				<p class="begintijd">12:00u - 3:00u</p>
-			</div>
-		</li>
-	</ul>
-</div>
-
 <!-- Dit zijn de navigatieknoppen voor de vorige/volgende lijst evenementen-->
-<div id="buttons">  
-	<span class="prev"></span>
-	<span class="next"></span>
-</div>
 
-<!-- De tabelweergave (week) van de evenementen-->
-<table id="agenda">
-	<tr><th>&nbsp;</th><th>maandag</th><th>dinsdag</th><th>woensdag</th><th>donderdag</th><th>vrijdag</th><th>zaterdag</th><th>zondag</th></tr>
-	<tr id="u0"><td>00:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u1"><td>01:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u2"><td>02:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u3"><td>03:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u4"><td>04:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u5"><td>05:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u6"><td>06:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u7"><td>07:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u8"><td>08:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u9"><td>09:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u10"><td>10:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u11"><td>11:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u12"><td>12:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u13"><td>13:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u14"><td>14:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u15"><td>15:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u16"><td>16:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u17"><td>17:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u18"><td>18:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u19"><td>19:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u20"><td>20:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u21"><td>21:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u22"><td>22:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-	<tr id="u23"><td>23:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
-</table>
+<div id="weergave">
+	<a href="index.php?page=agenda&amp;view=blok">Blok weergave</a>
+	<a href="index.php?page=agenda&amp;view=lijst">Lijst weergave</a>
+</div>
+<?php
+
+if(!isset($_GET['view']) || empty($_GET['view']))
+{
+	$_GET['view'] = 'blok';
+}
+
+if($_GET['view'] != 'blok' && $_GET['view'] != 'lijst')
+{
+	$_GET['view'] = 'blok';
+}
+
+include('agenda_week_'.$_GET['view'].'.php');
+
+?>
---
+++ htdocs/agenda_week_blok.php
@@ -0,0 +1,44 @@
+<div id="buttons">  
+	<span class="prev"></span>
+	<span class="next"></span>
+</div>
+
+<!-- Deze form is voor het selecteren van de te tonen doelgroepen -->
+<form id="doelgroepen" action="">
+	<fieldset>
+	<legend>Categorie&euml;n</legend>
+	<label for="doelgroep_klant"><input type="checkbox" name="doelgroep" id="doelgroep_klant" value="Klant"  checked="checked" />Klant</label><br />
+	<label for="doelgroep_kok"><input type="checkbox" name="doelgroep" id="doelgroep_kok" value="Kok" checked="checked" />Keuken</label><br />
+	<label for="doelgroep_afwas"><input type="checkbox" name="doelgroep" id="doelgroep_afwas" value="Afwas" checked="checked" />Afwassers</label><br />
+	<label for="doelgroep_bar"><input type="checkbox" name="doelgroep" id="doelgroep_bar" value="Bar" checked="checked" />Barpersoneel</label><br />
+	</fieldset>
+</form>
+
+<!-- De tabelweergave (week) van de evenementen-->
+<table id="agenda">
+	<tr><th>&nbsp;</th><th>maandag</th><th>dinsdag</th><th>woensdag</th><th>donderdag</th><th>vrijdag</th><th>zaterdag</th><th>zondag</th></tr>
+	<tr id="u0"><td>00:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u1"><td>01:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u2"><td>02:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u3"><td>03:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u4"><td>04:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u5"><td>05:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u6"><td>06:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u7"><td>07:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u8"><td>08:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u9"><td>09:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u10"><td>10:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u11"><td>11:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u12"><td>12:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u13"><td>13:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u14"><td>14:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u15"><td>15:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u16"><td>16:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u17"><td>17:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u18"><td>18:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u19"><td>19:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u20"><td>20:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u21"><td>21:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u22"><td>22:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+	<tr id="u23"><td>23:00</td><td>&nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td> &nbsp;</td><td>&nbsp; </td><td>&nbsp; </td><td>&nbsp;</td></tr>
+</table>
