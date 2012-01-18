2ea43a0bb8cedfc3cf49a289fd519e0c8fbc9cf7
Ivan Plantevin <ivan.p92@gmail.com>
2012-01-18 13:38:48 +0100

    nu wel goede semipage bij keuren

--- htdocs/keuren.php
+++ htdocs/keuren.php
@@ -12,7 +12,7 @@
                         </p>
                         <div class="event_details">
                                 <p class="event_titel">
-                                        <a class="event_link" href="index.php?page=evenement&amp;semipage=agenda_week">Titel van het event</a>
+                                        <a class="event_link" href="index.php?page=evenement&amp;semipage=keuren">Titel van het event</a>
                                 </p>
                                 <p class="begintijd">11:00u - 19:00u</p>
                         </div>
@@ -27,7 +27,7 @@
                         </p>
                         <div class="event_details">
                                 <p class="event_titel">
-                                        <a class="event_link" href="index.php?page=evenement&amp;semipage=agenda_week">Titel van het event</a>
+                                        <a class="event_link" href="index.php?page=evenement&amp;semipage=keuren">Titel van het event</a>
                                 </p>
                                 <p class="begintijd">12:00u - 3:00u</p>
                         </div>
