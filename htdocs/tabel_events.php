4c377e75dd9533e5f0b311864ac4f2b50a746b42
David Woudenberg <david_woudenberg@hotmail.com>
2012-01-18 16:05:23 +0100

    tabelletje met events

---
+++ htdocs/tabel_events.php
@@ -0,0 +1,39 @@
+<div class="user_events">
+	<table>
+		<tbody>
+			<tr>
+				<th>
+				Ongekeurde Evenementen
+				</th>
+				<th>
+				Goedgekeurde Evenementen
+				</th>
+				<th>
+				Afgekeurde Evenementen
+				</th>
+			</tr>
+			<tr>
+				<td>
+					<ul>
+					<li>
+						<a href="index.php?page=evenement">TEST</a>
+					</li>
+					</ul>
+				</td>
+				<td>
+					<ul>
+					<li>
+						<a href="index.php?page=evenement">TEST</a>
+					</li>
+					</ul>
+				</td>
+				<td>
+					<ul>
+					<li>
+						<a href="index.php?page=evenement">TEST</a>
+					</li>
+					</ul>
+				</td>
+		</tbody>
+	</table>
+</div>
