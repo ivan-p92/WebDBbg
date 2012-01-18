ca08dec5174fba2a8ebbf8440a6a4a31ea3e6599
Vincent Velthuis <v.velthuis@gmail.com>
2012-01-18 16:07:04 +0100

    errors uit registratie

--- htdocs/registratie.php
+++ htdocs/registratie.php
@@ -5,7 +5,7 @@ Alle velden zijn verplicht
 <table>
 <tbody>
 <tr>
-<td>Volledige naam: </td><td><input type="text" id="naam" name="naam" /></td>
+<td>Volledige naam: </td><td><input type="text" id="naamVolledig" name="naamVolledig" /></td>
 </tr>
 <tr>
 <td>Emailadres: </td><td><input type="text" id="mail" name="mail" /></td>
@@ -14,11 +14,11 @@ Alle velden zijn verplicht
 <td>Wachtwoord: </td><td><input type="password" id="pswd" name="pswd" /></td>
 </tr>
 <tr>
-<td>Wachtwoord nogmaals: </td><td><input type="password" id="pswd2" name="pswd2"></td>
+<td>Wachtwoord nogmaals: </td><td><input type="password" id="pswd2" name="pswd2" /></td>
 </tr>
 <tr>
 <td>&nbsp;</td><td class="submit_button">
-	<button type="submit" class="button" href="#">
+	<button type="submit" class="button">
 		<span class="right">
 		<span class="inner">Registreer</span></span>
 </button></td>
