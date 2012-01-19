a50714dd680437068881665a718f970a4c1c998a
Vincent Velthuis <v.velthuis@gmail.com>
2012-01-19 13:28:27 +0100

    contact versie 1

---
+++ htdocs/contact.php
@@ -0,0 +1,18 @@
+<h1>Contact</h1>
+<p>Heeft u vragen of opmerkingen, dan horen wij dat graag. <br />
+Laat een bericht bij ons achter en wij nemen zo spoedig mogelijk contact met u op.</p>
+
+<form id="contact">
+	<label>Naam</label>
+	<input required="" placeholder="Typ hier uw naam" name="contact_naam">
+	<label>Email</label>
+	<input required="" placeholder="Typ hier uw e-mail adres" name="contact_mail">
+	<label>Bericht</label>
+	<textarea required="" placeholder="Typ hier uw bericht" name="contact_message">
+
+	<span class="submit_button" id="submit_contact">
+		<button type="submit" class="button" href="#">
+			<span class="right">
+			<span class="inner">Verstuur bericht</span></span>
+	</button></span>
+</form>
