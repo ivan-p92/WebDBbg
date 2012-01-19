00f5b3a0b7b7ab8b7fc9fbed3dac3ec775c40062
Vincent Velthuis <v.velthuis@gmail.com>
2012-01-19 15:24:25 +0100

    sorteer/zoek lijst_gebruikers

--- htdocs/lijst_van_gebruikers.php
+++ htdocs/lijst_van_gebruikers.php
@@ -1,5 +1,17 @@
 <div class="userlist">
 	<h1>Lijst van gebruikers<h1>
+	<table>
+		<tr><td>Sorteren op:</td>
+		<td><select id="sorteer" name="sorteer">
+			<option value="-------">-------</option>
+			<option value="Naam">Naam</option>
+			<option value="Rechten">Rechten</option>
+			<option value="ID">ID</option>
+		</select></td>
+		<td>Hier checkboxes voor rechten</td>
+		<td id="zoek">Zoek: <input type="text" name="zoek"></td>
+		</tr>
+	</table>
 	<p>Klik op een gebruiker om gegevens van deze gebruiker te bekijken en zijn rechten aan te passen
 		<ul>
 		<li>
