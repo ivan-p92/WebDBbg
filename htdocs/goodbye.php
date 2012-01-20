333910a3c3de85c561a5a1744568aee33ee6aa9d
Ivan Plantevin <ivan.p92@gmail.com>
2012-01-20 15:45:36 +0100

    home link nu wel goed

--- htdocs/goodbye.php
+++ htdocs/goodbye.php
@@ -7,5 +7,6 @@ unset($_SESSION);
 <h1>Tot ziens!</h1>
 
 <p>U bent succesvol uitgelogd.<br />
-<a class="submit_button" href="index.php"><span class="right"><span class="inner">Home</span></span></a>
+<a class="submit_button" title="Home" href="index.php"><button class="button">
+<span class="right"><span class="inner">Home</span></span></button></a>
 </p>
