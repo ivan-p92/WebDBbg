22f11e54ce29d7c102ca0bf97f9cd3cef0259e05
freek boutkan <freek.boutkan@gmail.com>
2012-01-20 14:20:59 +0100

    lala

--- htdocs/test.php
+++ htdocs/test.php
@@ -1,5 +1,2 @@
 <?php
-
-include('functions.php');
-
-printf("%s %s\n%s %s\n%s %s\n%s %s\n", 'freek', Functions::hashPass('freek'), 'ivan', Functions::hashPass('ivan'), 'vincent', Functions::hashPass('vincent'), 'david', Functions::hashPass('david'));
+phpinfo();
