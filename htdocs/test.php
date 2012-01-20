067418b30f7cbd9bc7487945dc73c96a563aaa8b
freek boutkan <freek.boutkan@gmail.com>
2012-01-20 14:24:04 +0100

    lala

--- htdocs/test.php
+++ htdocs/test.php
@@ -1,2 +1,17 @@
 <?php
-phpinfo();
+//phpinfo();
+
+mysql_connect('localhost', 'webdb1235', 'sadru2ew');
+mysql_select_db('webdb1235');
+
+$res = mysql_query("SELECT NOW()");
+if(!$res)
+{
+	echo'foutmelding';
+}
+else
+{
+	$la = mysql_fetch_assoc($res);
+	var_dump($la);
+}
+	
