f38f03f678c7d63a0786a42b7e0889603e1de63d
freek boutkan <freek.boutkan@gmail.com>
2012-01-20 11:04:04 +0100

    userlist

--- htdocs/lijst_van_gebruikers.php
+++ htdocs/lijst_van_gebruikers.php
@@ -1,34 +1,49 @@
 <div class="userlist">
 	<table id="sort_table">
-		<tr><td id="sort_name">Sorteren op:</td>
-		<td id="sorteer"><select name="sorteer">
-			<option value="-------">-------</option>
-			<option value="Naam">Naam</option>
-			<option value="Rechten">Rechten</option>
-			<option value="ID">ID</option>
-		</select></td>
-		<td id="recht_checkbox"><form action="" method="post"><ul>
-			<li><input type="checkbox" value="aanmaken" />Aanmaken</li>
-			<li><input type="checkbox" value="keuren" />Keuren</li>
-			<li><input type="checkbox" value="admin" />Admin</li>
-		</ul></form></td>
-		<td id="zoek"><span id="zoek_text">Zoek:</span><input type="text" id="zoek_box" name="zoek" /></td>
+		<tr>
+			<td id="sort_name">
+				Sorteren op:
+			</td>
+			<td id="sorteer">
+				<select name="sorteer">
+				<option value="-------">-------</option>
+				<option value="Naam">Naam</option>
+				<option value="Rechten">Rechten</option>
+				<option value="ID">ID</option>
+				</select>
+			</td>
+			<td id="recht_checkbox">
+				<form action="" method="post">
+					<ul>
+						<li><input type="checkbox" value="aanmaken" />Aanmaken</li>
+						<li><input type="checkbox" value="keuren" />Keuren</li>
+						<li><input type="checkbox" value="admin" />Admin</li>
+					</ul>
+				</form>
+			</td>
+			<td id="zoek">
+				<span id="zoek_text">Zoek:</span>
+				<input type="text" id="zoek_box" name="zoek" />
+			</td>
 		</tr>
 	</table>
+	
 	<h1>Lijst van gebruikers</h1>
-	<p>Klik op een gebruiker om gegevens van deze gebruiker te bekijken en zijn rechten aan te passen</p>
-		<ul>
+		<p>
+			Klik op een gebruiker om gegevens van deze gebruiker te bekijken en zijn rechten aan te passen
+		</p>
+	<ul id="userlistlist">
 		<li>
-		<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">Freek Boutkan</a>
+			<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">Freek Boutkan</a>
 		</li>
 		<li>
-		<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">Ivan Plantevin</a>
+			<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">Ivan Plantevin</a>
 		</li>
 		<li>
-		<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">Vincent Velthuis</a>
+			<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">Vincent Velthuis</a>
 		</li>
 		<li>
-		<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">David Woudenberg</a>
+			<a href="index.php?page=admin&amp;semipage=lijst_van_gebruikers">David Woudenberg</a>
 		</li>
-		</ul>	
+	</ul>	
 </div> 
