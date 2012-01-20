30af6ea2699295ae126c5b07b456364e41cd0bbf
freek boutkan <freek.boutkan@gmail.com>
2012-01-20 15:05:59 +0100

    lala2

--- htdocs/login.php
+++ htdocs/login.php
@@ -17,18 +17,17 @@ $stmt = $db->prepare($sql);
 $stmt->bindParam(':email', $_POST['naam']);
 $stmt->bindParam(':pass', Functions::hashPass($_POST['pwd']));
 $stmt->execute();
-var_dump($stmt);
-die();
+
 
 if($stmt->rowCount() == 1)
 {
-	$row = $stmt->fetch();
-	
+	$row = $stmt->fetch();	
 	$_SESSION['userid'] = $row['id'];
-
 }
 else
 {
+	echo $stmt->rowCount();
+	die();
 	header('Location: index.php?notice=invalid_login');
 }
 
