30265720112ebaefbb283cb17d1d607dce82a5f2
freek boutkan <freek.boutkan@gmail.com>
2012-01-20 14:14:53 +0100

    lala

--- htdocs/login.php
+++ htdocs/login.php
@@ -10,9 +10,11 @@ if(!isset($_POST['naam']) || !isset($_POST['pwd']) || empty($_POST['naam']) || e
 	header('Location: index.php?notice=incomplete_form');
 	die();
 }
-
+$sql = "SELECT id FROM users WHERE email = 'freek.boutkan@gmail.com' AND password = '6c56b463e8057d3ea083d783478701ebce00a0de';";
 $db = Functions::getDB();
-$stmt = $db->prepare("SELECT id FROM users WHERE email = 'freek.boutkan@gmail.com' AND password = '6c56b463e8057d3ea083d783478701ebce00a0de';");
+$stmt = $db->prepare($sql);
+
+//$stmt = $db->prepare("SELECT id FROM users WHERE email = 'freek.boutkan@gmail.com' AND password = '6c56b463e8057d3ea083d783478701ebce00a0de';");
 //$pwd = Functions::hashPass($_POST['pwd']);
 //$stmt->bind_param('ss', $_POST['naam'], $pwd);
 $stmt->execute();
