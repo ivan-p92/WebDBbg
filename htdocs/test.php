7ea730d96e0bf952a91e79e7e7d8ca98bbd4f7f2
freek boutkan <freek.boutkan@gmail.com>
2012-01-20 14:30:37 +0100

    lala

--- htdocs/test.php
+++ htdocs/test.php
@@ -14,9 +14,24 @@ else
 	$la = mysql_fetch_assoc($res);
 	var_dump($la);
 }
-	*/
+	
 	
 
 $db = new mysqli('localhost', 'webdb1235', 'sadru2ew', 'webdb1235');
 $res = $db->query("SELECT NOW();");
-var_dump($res);
+var_dump($res);*/
+
+$dsn = 'mysql:dbname=webdb1235;host=localhost';
+$user = 'webdb1235';
+$password = 'sadru2ew';
+
+try
+{
+	$db = new PDO($dsn, $user, $password);
+}
+catch (PDOException $e)
+{
+	echo 'Connection failed: ' . $e->getMessage();
+}
+
+var_dump($db->query("SELECT NOW();"));
