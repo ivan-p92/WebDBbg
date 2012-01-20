f49e16ee1a642183a61fb71e4a5af7e7b2d20e5d
Ivan Plantevin <ivan.p92@gmail.com>
2012-01-20 17:56:19 +0100

    nu wel goede semipage bij keuren.php

--- htdocs/keuren.php
+++ htdocs/keuren.php
@@ -60,7 +60,7 @@
 					
 					echo '<div class="event_details">';
 					echo '<p class="event_titel">';
-					echo '<a class="event_link" href="index.php?page=evenement&amp;id='.$row['id'].'&amp;semipage=agenda_week">'.$row['title'].'</a>';
+					echo '<a class="event_link" href="index.php?page=evenement&amp;id='.$row['id'].'&amp;semipage=keuren">'.$row['title'].'</a>';
 					echo '</p>';
 					echo '<p class="begintijd">Begin: '.$row['begin_tijd'].'u. Eind: '.$row['eind_tijd'].'u. @'.$row['location'].'</p>';
 					echo '</div>';
@@ -78,7 +78,7 @@
 
                                         echo '<div class="event_details">';
                                         echo '<p class="event_titel">';
-                                        echo '<a class="event_link" href="index.php?page=evenement&amp;id='.$row['id'].'&amp;semipage=agenda_week">'.$row['title'].'</a>';
+                                        echo '<a class="event_link" href="index.php?page=evenement&amp;id='.$row['id'].'&amp;semipage=keuren">'.$row['title'].'</a>';
                                         echo '</p>';
                                         echo '<p class="begintijd">Begin: '.$row['begin_tijd'].'u. Eind: '.$row['eind_tijd'].'u. @'.$row['location'].'</p>';
                                         echo '</div>';
