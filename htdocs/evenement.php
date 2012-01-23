578a23555b673ed8be2ba748ee97124074fa9cc5
Ivan Plantevin <ivan.p92@gmail.com>
2012-01-23 22:46:59 +0100

    evenement nu ook vanuit agenda en verder beveiligd met isset

--- htdocs/evenement.php
+++ htdocs/evenement.php
@@ -1,5 +1,5 @@
 <?php
-if($_GET["semipage"]=="toevoeg_evenement" && Functions::auth("submit_event"))
+if($_GET["semipage"]=="toevoeg_evenement" && Functions::auth("submit_event") && !empty($_POST))
 {
 echo'
 <h1>Evenement</h1>
@@ -69,8 +69,8 @@ echo'
 
 </div>';
 }
-elseif($_GET["semipage"]=="keuren" && Functions::auth("approve_event"))
 
+elseif($_GET["semipage"]=="keuren" && Functions::auth("approve_event") && isset($_GET["id"]))
 {
 $database=Functions::getDB();
 
@@ -139,8 +139,21 @@ echo'
 </div>';
 } 
 
-else
+elseif(($_GET["semipage"]=="agenda_week_lijst" || $_GET["semipage"]=="agenda_week_blok") && isset($_GET["id"]))
+
 {
+$database=Functions::getDB();
+
+$sql = 'SELECT events.*, users.name FROM events INNER JOIN users ON users.id=events.create_id WHERE events.id=:id';
+
+$stmt = $database->prepare($sql);
+
+$stmt->bindParam(":id", $_GET["id"], PDO::PARAM_INT);
+
+$stmt->execute();
+
+$info=$stmt->fetch();
+
 echo'
 <h1>Evenement</h1>
 
@@ -148,36 +161,27 @@ echo'
 	<tbody>
 	<tr>
 		<td>Titel</td>
-		<td class="rechts">Hier komt de titel</td>
+		<td class="rechts">'.$info["title"].'</td>
 	</tr>
 	<tr>
 		<td>Plaatsing</td>
-		<td class="rechts">Datum van toevoegen en auteur</td>
-	</tr>
-	<tr>
-		<td>Goedkeuring</td>
-		<td class="rechts">Datum van goedkeuren en autorisator</td>
+		<td class="rechts">'."Op ".$info["create_date"]." door ".$info["name"].'</td>
 	</tr>
 	<tr>
 		<td>Omschrijving</td>
-		<td class="rechts">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-			Mauris vel magna. Mauris risus nunc, tristique varius, gravida in, 
-			lacinia vel, elit.Nam ornare, felis non faucibus molestie, 
-			nulla augue adipiscing mauris, a nonummy diam ligula ut risus. 
-			Praesent varius. Cum sociis natoque penatibus et magnis dis parturient montes, 
-			nascetur ridiculus mus.</td>
+		<td class="rechts">'.$info["description"].'.</td>
 	</tr>
 	<tr>
 		<td>Locatie</td>
-		<td>Hier komt de locatie</td>
+		<td>'.$info["location"].'</td>
 	</tr>
 	<tr>
 		<td>Begintijd</td>
-		<td class="rechts">Hier komt de begintijd en datum</td>
+		<td class="rechts">'.$info["start_date"].'</td>
 	</tr>
 	<tr>
 		<td>Eindtijd</td>
-		<td class="rechts">Hier de eindtijd en datum</td>
+		<td class="rechts">'.$info["end_date"].'</td>
 	</tr>
 	<tr>
 		<td rowspan="4">Categorie</td>
@@ -195,17 +199,14 @@ echo'
 	</tbody>
 </table>
 
-<div id="event_buttons">
-<a class="submit_button" href="#" title="Goedkeuren">
-	<button class="button"><span class="right"><span class="inner">Goedkeuren</span></span></button>
-</a>	
-<a class="submit_button" href="#" title="Afkeuren">
-        <button class="button"><span class="right"><span class="inner">Afkeuren</span></span></button>
-</a>
-<a class="submit_button" href="#" title="Aanpassen">
-        <button class="button"><span class="right"><span class="inner">Aanpassen</span></span></button>
-</a>
-
-</div>';
+';
+} 
+else
+{
+	echo'
+	<h1>Fout!</h1>
+	<p>U hebt geen rechten om deze pagina te bekijken of benadert de pagina op de verkeerde manier. Controleer uw invoer of log in.</p>
+	';
 }
+
 ?>
