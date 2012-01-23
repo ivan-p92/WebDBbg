02487497472075978805ffd8a73971f16b78fa55
David Woudenberg <david_woudenberg@hotmail.com>
2012-01-23 16:40:25 +0100

    mogelijkheid om groepen aan evenementen toe te voegen nu ook met database

--- htdocs/data_verstuur.php
+++ htdocs/data_verstuur.php
@@ -27,6 +27,20 @@ $database = Functions::getDB(); /*new mysqli('localhost', 'webdb1235', 'sadru2ew
 	
 	$stmt->execute();
 	
+	$event_id=$database->latInsertId();
+	
+	$sql='INSERT INTO events_groups (event_id, group_id) VALUES (:event_id, (SELECT id FROM groups WHERE group=:group))';
+	
+	$stmt2=database->prepare(sql);
+	
+	foreach($_SESSION["tijdelijke_evenementwaardes"]["categorie"] as $groep)
+	{
+		$stmt->bindParam(':event_id', $event_id, PDO::PARAM_INT);
+		$stmt->bindParam(':group', $groep, PDO::PARAM_STR);
+		
+		$stmt->execute();
+	}
+	
 	echo'<meta http-equiv="refresh" content="0; url=http://websec.science.uva.nl/webdb1235/index.php" />';
 }
 
