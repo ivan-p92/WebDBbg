c96dcd7e966cfa08a30e0fc2754c8edc49694d71
David Woudenberg <david_woudenberg@hotmail.com>
2012-01-23 17:04:13 +0100

    onjuiste code eruit

--- htdocs/data_verstuur.php
+++ htdocs/data_verstuur.php
@@ -27,7 +27,7 @@ $database = Functions::getDB(); /*new mysqli('localhost', 'webdb1235', 'sadru2ew
 	
 	$stmt->execute();
 	
-	$event_id=$database->lastInsertId();
+	/*$event_id=$database->lastInsertId();
 	
 	$sql='INSERT INTO events_groups (event_id, group_id) VALUES (:event_id, (SELECT id FROM groups WHERE group=:group))';
 	
@@ -39,7 +39,7 @@ $database = Functions::getDB(); /*new mysqli('localhost', 'webdb1235', 'sadru2ew
 		$stmt->bindParam(':group', $groep, PDO::PARAM_STR);
 		
 		$stmt->execute();
-	}
+	}*/
 	
 	echo'<meta http-equiv="refresh" content="0; url=http://websec.science.uva.nl/webdb1235/index.php" />';
 }
