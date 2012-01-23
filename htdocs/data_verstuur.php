c1b0eee6e5e23855c2b71a44b33b63c19dc7dd61
David Woudenberg <david_woudenberg@hotmail.com>
2012-01-23 17:25:14 +0100

    groepen

--- htdocs/data_verstuur.php
+++ htdocs/data_verstuur.php
@@ -27,9 +27,9 @@ $database = Functions::getDB(); /*new mysqli('localhost', 'webdb1235', 'sadru2ew
 	
 	$stmt->execute();
 	
-	/*$event_id=$database->lastInsertId();
+	$event_id=$database->lastInsertId();
 	
-	$sql='INSERT INTO events_groups (event_id, group_id) VALUES (:event_id, (SELECT id FROM groups WHERE group=:group))';
+	$sql='INSERT INTO events_groups (event_id, group_id) VALUES (:event_id, (SELECT id FROM groups WHERE `group`=:group))';
 	
 	$stmt=$database->prepare($sql);
 	
@@ -39,7 +39,7 @@ $database = Functions::getDB(); /*new mysqli('localhost', 'webdb1235', 'sadru2ew
 		$stmt->bindParam(':group', $groep, PDO::PARAM_STR);
 		
 		$stmt->execute();
-	}*/
+	}
 	
 	echo'<meta http-equiv="refresh" content="0; url=http://websec.science.uva.nl/webdb1235/index.php" />';
 }
