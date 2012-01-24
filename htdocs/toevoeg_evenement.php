e617d69b00da256f5c763b0ddda03dc8c0890170
David Woudenberg <david_woudenberg@hotmail.com>
2012-01-24 17:05:09 +0100

    volgende versie

--- htdocs/toevoeg_evenement.php
+++ htdocs/toevoeg_evenement.php
@@ -4,13 +4,13 @@
 if(Functions::auth("submit_event") && isset($_SESSION["tijdelijke_evenementwaardes"])) 
 {
 	echo'
-	<p>DEBUG Voeg hier een evenement toe, zorg ervoor dat alle velden worden ingevuld</p>
+	<p>Voeg hier een evenement toe, zorg ervoor dat alle velden worden ingevuld</p>
 	<div class="form"><form id="input" action="http://websec.science.uva.nl/webdb1235/index.php?page=evenement&amp;semipage=toevoeg_evenement" method="post">
 	<table class="formtable" id="event_toevoegen">
 		<tbody>
 		<tr>
 			<td id="eerstecel">Titel</td>
-			<td><input id="titel" type="text" name="titel" />'.$_SESSION["tijdelijke_evenementwaardes"]["titel"].'</td>
+			<td><input id="titel" type="text" name="titel" value="'._SESSION["tijdelijke_evenementwaardes"]["titel"].'" /></td>
 		</tr>
 		<tr id="omschrijving">
 			<td>Omschrijving</td>
@@ -154,37 +154,14 @@ if(Functions::auth("submit_event") && isset($_SESSION["tijdelijke_evenementwaard
 		<tr>
 			<td>Einddatum</td>
 			<td><select id="datum2" name="datum2">
-	<option value="1">1</option>
-	<option value="2">2</option>
-	<option value="3">3</option>
-	<option value="4">4</option>
-	<option value="5">5</option>
-	<option value="6">6</option>
-	<option value="7">7</option>
-	<option value="8">8</option>
-	<option value="9">9</option>
-	<option value="10">10</option>
-	<option value="11">11</option>
-	<option value="12">12</option>
-	<option value="13">13</option>
-	<option value="14">14</option>
-	<option value="15">15</option>
-	<option value="16">16</option>
-	<option value="17">17</option>
-	<option value="18">18</option>
-	<option value="19">19</option>
-	<option value="20">20</option>
-	<option value="21">21</option>
-	<option value="22">22</option>
-	<option value="23">23</option>
-	<option value="24">24</option>
-	<option value="25">25</option>
-	<option value="26">26</option>
-	<option value="27">27</option>
-	<option value="28">28</option>
-	<option value="29">29</option>
-	<option value="30">30</option>
-	<option value="31">31</option>
+	';
+		$datum2 = $_SESSION["tijdelijke_evenementwaardes"]["datum2"];
+		for($i=1; $i<=31; $i++)
+		{
+			if($i == $datum2) echo'<option value="'.$i.'" selected="selected">'.$i.'</option>';
+			else echo'<option value="'.$i.'">'.$i.'</option>';
+		}
+	echo'
 	</select>
 	<select name="maand2">
 	<option value="01">januari</option>
@@ -314,21 +291,29 @@ if(Functions::auth("submit_event") && isset($_SESSION["tijdelijke_evenementwaard
 			Locatie
 			</td>
 			<td>
-			<input id="locatie" type="text" name="locatie" />
+			<input id="locatie" type="text" name="locatie" value="'.$_SESSION["tijdelijke_evenementwaardes"]["locatie"].'" />
 			</td>
 		</tr>
 		<tr>
 			<td rowspan="4">Categorie</td>
-			<td><input type="checkbox" value="klant" name="categorie[]" />Klant</td>
+			<td><input type="checkbox" value="klant" name="categorie[]"';  
+				if(inarray("klant", $_SESSION["tijdelijke_evenementwaardes"]["categorie"])echo'checked="checked"';
+					echo'/>Klant</td>
 		</tr>
 		<tr>
-			<td><input type="checkbox" value="keuken" name="categorie[]" />Keuken</td>
+			<td><input type="checkbox" value="keuken" name="categorie[]"';  
+				if(inarray("keuken", $_SESSION["tijdelijke_evenementwaardes"]["categorie"])echo'checked="checked"';
+					echo'/>Keuken</td>
 		</tr>
 		<tr>
-			<td><input type="checkbox" value="afwas" name="categorie[]" />Afwassers</td>
+			<td><input type="checkbox" value="afwas" name="categorie[]"';  
+				if(inarray("afwas", $_SESSION["tijdelijke_evenementwaardes"]["categorie"])echo'checked="checked"';
+					echo'/>Afwassers</td>
 		</tr>
 		<tr>
-			<td><input type="checkbox" value="bar" name="categorie[]" />Barpersoneel</td>
+			<td><input type="checkbox" value="bar" name="categorie[]"';  
+				if(inarray("bar", $_SESSION["tijdelijke_evenementwaardes"]["categorie"])echo'checked="checked"';
+					echo'/>Barpersoneel</td>
 		</tr>
 		<tr>
 			<td>&nbsp</td>
