f1fc98f8ca7d3e59cf11fad39984345d45679b5d
Vincent Velthuis <v.velthuis@gmail.com>
2012-01-24 12:23:22 +0100

    test admin rechten veranderen

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -9,14 +9,8 @@ if(Functions::auth("admin_rights"))
 		$stmt = $mysqli->prepare($sql);
 		$stmt->execute();
 				
-		$sql_check = "SELECT permission_id FROM users_permissions WHERE user_id=" . $_GET['id'] . ";";
-		$stmt_check = $mysqli->prepare($sql_check);
-		$stmt_check->execute();
-		?>
-				
-	<div class="admin">
+	echo '<div class="admin">'
 		
-		<?php
 		
 		if($stmt->rowCount() == 0)
 		{
@@ -24,106 +18,124 @@ if(Functions::auth("admin_rights"))
 		}
 		elseif(!empty($_POST))
 		{
-			echo 'tralalalalala';
+			$sql_submit = "DELETE * FROM users_permissions WHERE user_id=:user_id;";
+			$stmt_submit = $mysqli->prepare($sql_submit);
+			$stmt_submit->bindParam(':user_id',$_GET['id'],PDO::PARAM_INT);
+			$stmt_submit->execute();
+			
+			$sql_submit2 = "INSERT into users_permissions (user_id, permission_id) VALUES (:user_id,
+						    (SELECT id FROM permissions WHERE permission = :permission));";
+			$stmt_submit2 = $mysqli->prepare($sql_submit2);
+			
+			foreach( $_POST['admin_check'] as $recht )
+			{
+				$stmt_submit2->bindParam(':user_id',$_GET['id'],PDO::PARAM_INT);
+				$stmt_submit2->bindParam(':permission', $recht, PDO::PARAM_STR);
+				$stmt_submit2->execute();
+			}
 		}
 		elseif(empty($_POST))
 		{
+						
+			$sql_check = "SELECT permission_id FROM users_permissions WHERE user_id=" . $_GET['id'] . ";";
+			$stmt_check = $mysqli->prepare($sql_check);
+			$stmt_check->execute();
+				?>
+			
+			<script type="text/javascript">
+			function check_rechten( id )
+			{
+				switch(id)
+				{
+				case 1:
+					document.getElementById("submit_event").checked="checked";
+					break;
+				case 2:
+					document.getElementById("approve_event").checked="checked";
+					break;
+				case 3:
+					document.getElementById("admin_rights").checked="checked";
+					break;
+				default:
+					document.write("Permission_id niet correct");
+				}
+			}
+			
+			function init()
+			{
+			<?php
+				while($check = $stmt_check->fetch())
+				{
+					echo "check_rechten(".$check['permission_id'].");\n";
+				}
 			?>
+			}
 			
-		<script type="text/javascript">
-		function check_rechten( id )
-		{
-			switch(id)
-			{
-			case 1:
-				document.getElementById("create").checked="checked";
-				break;
-			case 2:
-				document.getElementById("approve").checked="checked";
-				break;
-			case 3:
-				document.getElementById("admin1").checked="checked";
-				break;
-			default:
-				document.write("Permission_id niet correct");
-			}
-		}
-		
-		function init()
-		{
-		<?php
-			while($check = $stmt_check->fetch())
-			{
-				echo "check_rechten(".$check['permission_id'].");\n";
-			}
-		?>
-		}
-		
-		document.addEventListener("DOMContentLoaded", init, false);
-		</script>
-		
-		<?php
-			$row = $stmt->fetch();
-			echo'
-			<h1>Admin pagina van '.$row['name'].'</h1>
-				<p>U kunt hier zien welke evenementen door '.$row['name'].' aangemaakt/goedgekeurd/afgekeurd zijn.<br />
-					Ook kunt u diens rechten hier aanpassen.</p>
-				<p>De volgende evenementen zijn door '.$row['name'].' aangemaakt:</p>
-				
-				<div class="user_events">
-					<table id="user_events">
-						<tbody>
-							<tr>
-								<th>
-								Ongekeurde Evenementen
-								</th>
-								<th>
-								Goedgekeurde Evenementen
-								</th>
-								<th>
-								Afgekeurde Evenementen
-								</th>
-							</tr>
-							<tr>
-								<td>
-									<ul>
-									<li>
-										<a href="index.php?page=evenement">TEST</a>
-									</li>
-									</ul>
-								</td>
-								<td>
-									<ul>
-									<li>
-										<a href="index.php?page=evenement">TEST</a>
-									</li>
-									</ul>
-								</td>
-								<td>
-									<ul>
-									<li>
-										<a href="index.php?page=evenement">TEST</a>
-									</li>
-									</ul>
-								</td>
-						</tbody>
-					</table>
-				</div>
+			document.addEventListener("DOMContentLoaded", init, false);
+			</script>
+			
+			<?php
+				$row = $stmt->fetch();
+				echo'
+				<h1>Admin pagina van '.$row['name'].'</h1>
+					<p>U kunt hier zien welke evenementen door '.$row['name'].' aangemaakt/goedgekeurd/afgekeurd zijn.<br />
+						Ook kunt u diens rechten hier aanpassen.</p>
+					<p>De volgende evenementen zijn door '.$row['name'].' aangemaakt:</p>
+					
+					<div class="user_events">
+						<table id="user_events">
+							<tbody>
+								<tr>
+									<th>
+									Ongekeurde Evenementen
+									</th>
+									<th>
+									Goedgekeurde Evenementen
+									</th>
+									<th>
+									Afgekeurde Evenementen
+									</th>
+								</tr>
+								<tr>
+									<td>
+										<ul>
+										<li>
+											<a href="index.php?page=evenement">TEST</a>
+										</li>
+										</ul>
+									</td>
+									<td>
+										<ul>
+										<li>
+											<a href="index.php?page=evenement">TEST</a>
+										</li>
+										</ul>
+									</td>
+									<td>
+										<ul>
+										<li>
+											<a href="index.php?page=evenement">TEST</a>
+										</li>
+										</ul>
+									</td>
+							</tbody>
+						</table>
+					</div>
 
-				<p>Op dit moment heeft '.$row['name'].' de volgende rechten:</p>
-				<form id="admin" action="index.php?page=admin&amp;semipage=lijst_van_gebruikers&amp;id='.$_GET['id'].'" method="post">
+					<p>Op dit moment heeft '.$row['name'].' de volgende rechten:</p>
+					<form id="admin" action="index.php?page=admin&amp;semipage=lijst_van_gebruikers&amp;id='.$_GET['id'].'" method="post">
+					
+					<input type="checkbox" name="admin_check[]" value="submit_event" id="create" />Evenementen aanmaken
+					<input type="checkbox" name="admin_check[]" value="approve_event" id="approve" />Evenementen keuren
+					<input type="checkbox" name="admin_check[]" value="admin_rights" id="admin1" />Admin rechten<br />';
+					
 				
-				<input type="checkbox" name="admin_check[]" value="create" id="create" />Evenementen aanmaken
-				<input type="checkbox" name="admin_check[]" value="approve" id="approve" />Evenementen keuren
-				<input type="checkbox" name="admin_check[]" value="admin" id="admin1" />Admin rechten<br />';
-				
-			
-				
-				echo' <span id="submit_rechten" class="submit_button"><button class="button" type="submit">
-						<span class="right">
-						<span class="inner">Pas rechten aan</span></span>
-				</button>
-				</form>';
+					
+					echo' <span id="submit_rechten" class="submit_button"><button class="button" type="submit">
+							<span class="right">
+							<span class="inner">Pas rechten aan</span></span>
+					</button>
+					</form>';
 		}
 	}
 	catch(Exception $exception)
