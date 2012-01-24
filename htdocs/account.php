8deea446413c2b153d73b4ed0ce80c9b9bf76144
freek boutkan <freek.boutkan@gmail.com>
2012-01-24 13:57:20 +0100

    accountinformatie

--- htdocs/account.php
+++ htdocs/account.php
@@ -49,159 +49,124 @@ else
 		{
 			$userInfo = $stmtUserInfo->fetch();
 ?>
-		<h1>Account informatie</h1>
+			<h1>Account informatie</h1>
 
-		<p>
-			Op deze pagina vindt u informatie over uw account. <br />
-			U kunt in het onderstaande formulier ook uw wachtwoord wijzigen.
-		</p>
-		
-		<h3 class="big nopadding">Persoonlijke informatie</h3>
 
-		<p class="nomargintop">
-			<span class="b block paddingtop">Naam:</span>
-			<?php echo $userInfo['name']; ?><br />
-			<span class="b block paddingtop">Accountnaam (email):</span>
-			<?php echo $userInfo['email']; ?><br />
-			<span class="b block paddingtop">Huidige permissies:</span>
-			<?php
-			if($stmtUserRights->rowCount() == 0)
+			
+			<h3 class="big nopadding">Persoonlijke informatie</h3>
+
+			<p class="nomargintop">
+				<span class="b block paddingtop">Naam:</span>
+				<?php echo $userInfo['name']; ?><br />
+				<span class="b block paddingtop">Accountnaam (email):</span>
+				<?php echo $userInfo['email']; ?><br />
+				<span class="b block paddingtop">Huidige permissies:</span>
+				<?php
+				if($stmtUserRights->rowCount() == 0)
+				{
+					echo 'Geen rechten';
+				}
+				else
+				{
+					while($rightsRow = $stmtUserRights->fetch())
+					{
+						echo translateRights($rightsRow['permission']).'<br />';
+					}
+				}
+			echo '</p>';
+			
+			echo '<h3 class="big nopadding">Deze evenementen zijn door u aangemaakt:</h3>';	
+			
+			$sqlEvents = "SELECT * FROM `events_status` WHERE create_id = :user_id ORDER BY `status` ASC, `create_date` DESC";
+			$stmtEvents = $db->prepare($sqlEvents);
+			$stmtEvents->bindParam(':user_id', $_GET['id'], PDO::PARAM_INT);
+			$stmtEvents->execute();
+			
+			if($stmtEvents->rowCount() == 0)
 			{
-				echo 'Geen rechten';
+				echo '<p class="nomargintop">U heeft nog geen events aangemaakt</p>';
 			}
 			else
 			{
-				while($rightsRow = $stmtUserRights->fetch())
+				$events = array('approved' => array(), 'unapproved' => array(), 'declined' => array());
+				while($row = $stmtEvents->fetch())
 				{
-					echo translateRights($rightsRow['permission']).'<br />';
+					$events[$row['status']][] = $row;
 				}
-			}
+						
+				if(count($events['unapproved']) != 0)
+				{
+					echo '<span class="b block paddingtop">Nog te keuren:</span>';
+					foreach($events['unapproved'] as $value)
+					{
+						echo $value['title'].'<br />';
+					}
+				}
+				
+				if(count($events['approved']) != 0)
+				{
+					echo '<span class="b block paddingtop">Goedgekeurde evenementen:</span>';
+					foreach($events['approved'] as $value)
+					{
+						echo $value['title'].'<br />';
+					}
+				}
+				
+				if(count($events['declined']) != 0)
+				{
+					echo '<span class="b block paddingtop">Afgekeurde evenementen:</span>';
+					foreach($events['declined'] as $value)
+					{
+						echo $value['title'].'<br />';
+					}
+				}
 			?>
-		</p>
-		
-		<?php
-		
-		echo '<h3 class="big nopadding">Deze evenementen zijn door u aangemaakt:</h3>';	
-		
-		$sqlEvents = "SELECT * FROM `events_status` WHERE create_id = :user_id ORDER BY `status` ASC, `create_date` DESC";
-		$stmtEvents = $db->prepare($sqlEvents);
-		$stmtEvents->bindParam(':user_id', $_GET['id'], PDO::PARAM_INT);
-		$stmtEvents->execute();
-		
-		if($stmtEvents->rowCount() == 0)
-		{
-			echo '<p class="nomargintop">U heeft nog geen events aangemaakt</p>';
-		}
-		else
-		{
-			$events = array('approved' => array(), 'unapproved' => array(), 'declined' => array());
-			while($row = $stmtEvents->fetch())
-			{
-				$events[$row['status']][] = $row;
-			}
-					
-			if(count($events['unapproved']) != 0)
-			{
-				echo '<span class="b block paddingtop">Nog te keuren:</span>';
-				foreach($events['unapproved'] as $value)
-				{
-					echo $value['title'].'<br />';
-				}
-			}
-			
-			if(count($events['approved']) != 0)
-			{
-				echo '<span class="b block paddingtop">Goedgekeurde evenementen:</span>';
-				foreach($events['approved'] as $value)
-				{
-					echo $value['title'].'<br />';
-				}
-			}
-			
-			if(count($events['declined']) != 0)
-			{
-				echo '<span class="b block paddingtop">Afgekeurde evenementen:</span>';
-				foreach($events['declined'] as $value)
-				{
-					echo $value['title'].'<br />';
-				}
-			}
-		?>
-			<!--
-			<p>Deze evenementen zijn door u aangemaakt:</p>
-			<div class="user_events">
-				<table id="user_events">
-					<tbody>
-						<tr>
-							<th>
-							Ongekeurde Evenementen
-							</th>
-							<th>
-							Goedgekeurde Evenementen
-							</th>
-							<th>
-							Afgekeurde Evenementen
-							</th>
-						</tr>
-						<tr>
-							<td>
-								<ul>
-								<li>
-									<a href="index.php?page=evenement">TEST</a>
-								</li>
-								</ul>
-							</td>
-							<td>
-								<ul>
-								<li>
-									<a href="index.php?page=evenement">TEST</a>
-								</li>
-								</ul>
-							</td>
-							<td>
-								<ul>
-								<li>
-									<a href="index.php?page=evenement">TEST</a>
-								</li>
-								</ul>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-			-->
-		<?php
-		}
-		?>
-
-		<form id="wijzigww" action="" method="post">
-			<table id="wijzigww_tabel">
-				<tbody>
-					<tr>
-						<td>Oud wachtwoord: </td>
-						<td><input type="password" id="oldpswd" name="mail" required="" /></td>
-					</tr>
-					<tr>
-						<td>Nieuw wachtwoord: </td>
-						<td><input type="password" id="newpswd" name="pswd" required="" /></td>
-					</tr>
-					<tr>
-						<td>Wachtwoord nogmaals: </td>
-						<td><input type="password" id="newpswd2" name="pswd2" required="" /></td>
-					</tr>
-					<tr>
-						<td>&nbsp;</td>
-						<td id="wijzigww_buttons" class="submit_button">
-							<button type="submit" class="button" id="wwopslaan"><span class="right"><span class="inner">Wachtwoord opslaan</span></span></button><br />
-							<button type="reset" class="button" id="wwreset"><span class="right"><span class="inner">Velden wissen</span></span></button>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</form>
-	
-<?php
-		}
+				<!--
+				<p>Deze evenementen zijn door u aangemaakt:</p>
+				<div class="user_events">
+					<table id="user_events">
+						<tbody>
+							<tr>
+								<th>
+								Ongekeurde Evenementen
+								</th>
+								<th>
+								Goedgekeurde Evenementen
+								</th>
+								<th>
+								Afgekeurde Evenementen
+								</th>
+							</tr>
+							<tr>
+								<td>
+									<ul>
+									<li>
+										<a href="index.php?page=evenement">TEST</a>
+									</li>
+									</ul>
+								</td>
+								<td>
+									<ul>
+									<li>
+										<a href="index.php?page=evenement">TEST</a>
+									</li>
+									</ul>
+								</td>
+								<td>
+									<ul>
+									<li>
+										<a href="index.php?page=evenement">TEST</a>
+									</li>
+									</ul>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				-->
+			<?php
+			}// End Else ~If(Er zijn geen events)
+		}// End Else ~If(user bestaat)
 	}
 	catch(Exception $e)
 	{
