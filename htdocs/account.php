ee49ceefdbf3a80b1c3785ee13e5bf4169d3ccb6
freek boutkan <freek.boutkan@gmail.com>
2012-01-24 11:47:56 +0100

    accountinformatie

--- htdocs/account.php
+++ htdocs/account.php
@@ -39,98 +39,113 @@ else
 		$stmtUserRights->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
 		$stmtUserRights->execute();
 		
-	}
+
 	
-	if($stmt->rowCount() != 1)
-	{
-		echo '<h1>Foutje</h1><p>Deze gebruiker bestaat niet (meer).</p>';
-	}
-	else
-	{
-		$userInfo = $stmt->fetch();
+		if($stmt->rowCount() != 1)
+		{
+			echo '<h1>Foutje</h1><p>Deze gebruiker bestaat niet (meer).</p>';
+		}
+		else
+		{
+			$userInfo = $stmt->fetch();
 ?>
+		<h1>Account informatie</h1>
 
+		<p> Op deze pagina vindt u informatie over uw account. <br />
+			U kunt in het onderstaande formulier ook uw wachtwoord wijzigen.</p>
 
-	<h1>Account informatie</h1>
+		<p>
+			Naam: <?php echo $userInfo['name']; ?><br />
+			Accountnaam (email): <?php echo $userInfo['email']; ?><br />
+			Huidige permissies: <?php
+								if($stmtUserRights->rowCount() == 0)
+								{
+									echo 'Geen rechten';
+								}
+								else
+								{
+									while($rightsRow = $stmtUserRights->fetch())
+									{
+										echo translateRights($rightsRow['permission']).'<br />';
+									}
+								}
+								?>
+		</p>
 
-	<p> Op deze pagina vindt u informatie over uw account. <br />
-		U kunt in het onderstaande formulier ook uw wachtwoord wijzigen.</p>
+		<p>Deze evenementen zijn door u aangemaakt:</p>
+		<div class="user_events">
+			<table id="user_events">
+				<tbody>
+					<tr>
+						<th>
+						Ongekeurde Evenementen
+						</th>
+						<th>
+						Goedgekeurde Evenementen
+						</th>
+						<th>
+						Afgekeurde Evenementen
+						</th>
+					</tr>
+					<tr>
+						<td>
+							<ul>
+							<li>
+								<a href="index.php?page=evenement">TEST</a>
+							</li>
+							</ul>
+						</td>
+						<td>
+							<ul>
+							<li>
+								<a href="index.php?page=evenement">TEST</a>
+							</li>
+							</ul>
+						</td>
+						<td>
+							<ul>
+							<li>
+								<a href="index.php?page=evenement">TEST</a>
+							</li>
+							</ul>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
 
-	<p>
-		Naam: <?php echo $userInfo['name']; ?><br />
-		Accountnaam (email): <?php echo $userInfo['email']; ?><br />
-		Huidige permissies: <?php while($rightsRow = $stmtUserRights->fetch()){ echo translateRights($rightsRow['permission']).'<br />';}?>
-	</p>
-
-	<p>Deze evenementen zijn door u aangemaakt:</p>
-	<div class="user_events">
-		<table id="user_events">
-			<tbody>
-				<tr>
-					<th>
-					Ongekeurde Evenementen
-					</th>
-					<th>
-					Goedgekeurde Evenementen
-					</th>
-					<th>
-					Afgekeurde Evenementen
-					</th>
-				</tr>
-				<tr>
-					<td>
-						<ul>
-						<li>
-							<a href="index.php?page=evenement">TEST</a>
-						</li>
-						</ul>
-					</td>
-					<td>
-						<ul>
-						<li>
-							<a href="index.php?page=evenement">TEST</a>
-						</li>
-						</ul>
-					</td>
-					<td>
-						<ul>
-						<li>
-							<a href="index.php?page=evenement">TEST</a>
-						</li>
-						</ul>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-
-	<form id="wijzigww" action="" method="post">
-		<table id="wijzigww_tabel">
-			<tbody>
-				<tr>
-					<td>Oud wachtwoord: </td>
-					<td><input type="password" id="oldpswd" name="mail" required="" /></td>
-				</tr>
-				<tr>
-					<td>Nieuw wachtwoord: </td>
-					<td><input type="password" id="newpswd" name="pswd" required="" /></td>
-				</tr>
-				<tr>
-					<td>Wachtwoord nogmaals: </td>
-					<td><input type="password" id="newpswd2" name="pswd2" required="" /></td>
-				</tr>
-				<tr>
-					<td>&nbsp;</td>
-					<td id="wijzigww_buttons" class="submit_button">
-						<button type="submit" class="button" id="wwopslaan"><span class="right"><span class="inner">Wachtwoord opslaan</span></span></button><br />
-						<button type="reset" class="button" id="wwreset"><span class="right"><span class="inner">Velden wissen</span></span></button>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-	</form>
+		<form id="wijzigww" action="" method="post">
+			<table id="wijzigww_tabel">
+				<tbody>
+					<tr>
+						<td>Oud wachtwoord: </td>
+						<td><input type="password" id="oldpswd" name="mail" required="" /></td>
+					</tr>
+					<tr>
+						<td>Nieuw wachtwoord: </td>
+						<td><input type="password" id="newpswd" name="pswd" required="" /></td>
+					</tr>
+					<tr>
+						<td>Wachtwoord nogmaals: </td>
+						<td><input type="password" id="newpswd2" name="pswd2" required="" /></td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+						<td id="wijzigww_buttons" class="submit_button">
+							<button type="submit" class="button" id="wwopslaan"><span class="right"><span class="inner">Wachtwoord opslaan</span></span></button><br />
+							<button type="reset" class="button" id="wwreset"><span class="right"><span class="inner">Velden wissen</span></span></button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</form>
 	
 <?php
+		}
+	}
+	catch(Exception $e)
+	{
+		echo '<h1>Foutje</h1><p>Excuses, technische fout</p>';
 	}
 }
 ?>
