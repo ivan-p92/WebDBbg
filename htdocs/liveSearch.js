a9ae0d80ffb6d11d85c31704475ddbd87a6dc347
freek boutkan <freek.boutkan@gmail.com>
2012-01-25 13:33:05 +0100

    js

--- htdocs/liveSearch.js
+++ htdocs/liveSearch.js
@@ -1,10 +1,12 @@
-function findPos(obj) {
+function findPos(obj)
+{
 	var curleft = curtop = 0;
 	if(obj.offsetParent) {
-	do {
-		curleft += obj.offsetLeft;
-		curtop += obj.offsetTop;
-	} while (obj = obj.offsetParent);
+		do {
+			curleft += obj.offsetLeft;
+			curtop += obj.offsetTop;
+		} while (obj = obj.offsetParent);
+	}
 	
 	return [curleft, curtop];
 }
