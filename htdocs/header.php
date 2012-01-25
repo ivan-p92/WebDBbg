0ccd06a5b5294f5dfb0e751e25a257fc69bde326
freek boutkan <freek.boutkan@gmail.com>
2012-01-25 13:56:16 +0100

    js

--- htdocs/header.php
+++ htdocs/header.php
@@ -6,6 +6,7 @@
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 	<link rel="stylesheet" type="text/css" href="css.css" title="CSS" />
 	<link rel="shortcut icon" type="image/x-ico" href="afbeeldingen/favicon.ico" />
+	<script type="text/javascript" src="jquery.js"></script>
 	<script type="text/javascript" src="liveSearch.js"></script>
 </head>
 
