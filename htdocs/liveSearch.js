1b49b866ce1a3d7416f2d208276aedc19bd93775
freek boutkan <freek.boutkan@gmail.com>
2012-01-25 13:49:53 +0100

    js

--- htdocs/liveSearch.js
+++ htdocs/liveSearch.js
@@ -1,25 +1,43 @@
-function findPos(obj)
-{
-	var curleft = curtop = 0;
-	
-	if(obj.offsetParent) {
-		do {
-			curleft += obj.offsetLeft;
-			curtop += obj.offsetTop;
-		} while (obj = obj.offsetParent);
-	}
+function findPosX(obj)
+  {
+    var curleft = 0;
+    if(obj.offsetParent)
+        while(1) 
+        {
+          curleft += obj.offsetLeft;
+          if(!obj.offsetParent)
+            break;
+          obj = obj.offsetParent;
+        }
+    else if(obj.x)
+        curleft += obj.x;
+    return curleft;
+  }
 
-	return [curleft, curtop];
-}
+  function findPosY(obj)
+  {
+    var curtop = 0;
+    if(obj.offsetParent)
+        while(1)
+        {
+          curtop += obj.offsetTop;
+          if(!obj.offsetParent)
+            break;
+          obj = obj.offsetParent;
+        }
+    else if(obj.y)
+        curtop += obj.y;
+    return curtop;
+  }
 
 
 function initLiveSearch()
 {
-	array = findPos(document.getElementById("zoek_box"));
+	el = document.getElementById("zoek_box");
 	box = document.getElementById("sresult");
 	
-	box.style.left = array[0]+"px"
-	box.style.top = array[1]+"px";
+	box.style.left = findPosX(el)+"px"
+	box.style.top = findPosX(el)+"px";
 }
 
 document.addEventListener("DOMContentLoaded", initLiveSearch, false);
