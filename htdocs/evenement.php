ac34484b9e56915b6dda1db6dc6a8662dc67ba80
Ivan Plantevin <ivan.p92@gmail.com>
2012-01-25 22:43:48 +0100

    alternatief

--- htdocs/evenement.php
+++ htdocs/evenement.php
@@ -107,7 +107,7 @@ elseif(isset($_GET["semipage"]) && $_GET["semipage"]=="toevoeg_evenement" && Fun
 		
 	// sql wordt gebruikt bij het berekenen van het verschil tussen de twee datums
 	$database = Functions::getDB();
-	$sql = 'SELECT TIMESTAMPDIFF(MINUTE,"2012-1-19 00:00:00", "2012-1-1 00:00:00") AS diff;';
+	$sql = 'SELECT TIMESTAMPDIFF(MINUTE,'.$begindatumtijd.','.$einddatumtijd.') AS diff;';
 	$stmt = $database->prepare($sql);
 	//$stmt->bindParam(1, $begindatumtijd, PDO::PARAM_STR);
 	//$stmt->bindParam(2, $einddatumtijd, PDO::PARAM_STR);
