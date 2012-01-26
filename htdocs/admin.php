bc098bda99bccbcae3df59f65dbef8905124f3bb
Vincent Velthuis <v.velthuis@gmail.com>
2012-01-26 12:46:18 +0100

    admin url beveiliging id=geen string

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -5,14 +5,15 @@ if(Functions::auth("admin_rights"))
 	{
 		$mysqli = Functions::getDB();
 		
-		$sql = "SELECT name FROM users WHERE id=" . $_GET['id'] . ";";
+		$sql = "SELECT name FROM users WHERE id=:user_id";
 		$stmt = $mysqli->prepare($sql);
+		$stmt->bindParam(":user_id",$_GET['id'],PDO::PARAM_INT);
 		$stmt->execute();
+		
 		$row = $stmt->fetch();
 				
 	echo '<div class="admin">';
-		
-		
+				
 		if($stmt->rowCount() == 0)
 		{
 			echo '<p>Deze gebruiker bestaat niet</p>';
@@ -25,7 +26,7 @@ if(Functions::auth("admin_rights"))
 			$stmt_delPermission->execute();
 			
 			$sql_addPermission = "INSERT into users_permissions (user_id, permission_id)
-							VALUES (:user_id,(SELECT id FROM permissions WHERE permission = :permission));";
+								  VALUES (:user_id,(SELECT id FROM permissions WHERE permission = :permission));";
 			$stmt_addPermission = $mysqli->prepare($sql_addPermission);
 			
 			foreach( $_POST['admin_check'] as $recht )
@@ -35,11 +36,10 @@ if(Functions::auth("admin_rights"))
 				$stmt_addPermission->execute();
 			}
 			echo '<h1>Succes!</h1>
-				<p>Rechten voor '.$row['name'].' aangepast.</p>';
+				  <p>Rechten voor '.$row['name'].' aangepast.</p>';
 		}
 		elseif(empty($_POST))
-		{
-						
+		{	
 			$sql_check = "SELECT permission_id FROM users_permissions WHERE user_id=" . $_GET['id'] . ";";
 			$stmt_check = $mysqli->prepare($sql_check);
 			$stmt_check->execute();
@@ -78,102 +78,96 @@ if(Functions::auth("admin_rights"))
 			</script>
 			
 			<?php
-				echo'
-				<h1>Admin pagina van '.$row['name'].'</h1>
-					<p>U kunt hier zien welke evenementen door '.$row['name'].' aangemaakt/goedgekeurd/afgekeurd zijn.<br />
-						Ook kunt u diens rechten hier aanpassen.</p>
-					<p>De volgende evenementen zijn door '.$row['name'].' aangemaakt:</p>
-					
-					<div class="user_events">
-						<table id="user_events">
-							<tbody>
-								<tr>
-									<th>
-									Ongekeurde Evenementen
-									</th>
-									<th>
-									Goedgekeurde Evenementen
-									</th>
-									<th>
-									Afgekeurde Evenementen
-									</th>
-								</tr>
-								<tr>
-									<td class="admin_td">';
-										$sql_ongekeurd = "SELECT title,id FROM events_status WHERE create_id=".$_GET['id']." AND status='unapproved'";
-										$stmt_ongekeurd = $mysqli->prepare($sql_ongekeurd);
-										$stmt_ongekeurd->execute();
-										
-										if($stmt_ongekeurd->rowCount() == 0)
-										{
-											echo '<p>Geen events</p>';
-										}
-										else
-										{
-											echo '<p>';
-											while($on = $stmt_ongekeurd->fetch())
-											{
-												echo '<a href="index.php?page=evenement&amp;id='.$on['id'].'&amp;semipage=keuren">'.out($on['title']).'</a><br />';
-											}
-											echo '</p>';
-										}
-										echo '</td>
-									<td class="admin_td">';
-										$sql_goedgekeurd = "SELECT title,id FROM events_status WHERE create_id=".$_GET['id']." AND status='approved'";
-										$stmt_goedgekeurd = $mysqli->prepare($sql_goedgekeurd);
-										$stmt_goedgekeurd->execute();
-										
-										if($stmt_goedgekeurd->rowCount() == 0)
-										{
-											echo '<p>Geen events</p>';
-										}
-										else
-										{
-											echo '<p>';
-											while($goed = $stmt_goedgekeurd->fetch())
-											{
-												echo '<a href="index.php?page=evenement&amp;id='.$goed['id'].'&amp;semipage=agenda_week">'.out($goed['title']).'</a><br />';
-											}
-											echo '</p>';
-										}
-										echo '</td>
-									<td class="admin_td">';
-										$sql_afgekeurd = "SELECT title,id FROM events_status WHERE create_id=".$_GET['id']." AND status='declined'";
-										$stmt_afgekeurd = $mysqli->prepare($sql_afgekeurd);
-										$stmt_afgekeurd->execute();
-										
-										if($stmt_afgekeurd->rowCount() == 0)
-										{
-											echo '<p>Geen events</p>';
-										}
-										else
-										{	
-											echo '<p>';
-											while($af = $stmt_afgekeurd->fetch())
-											{
-												echo ''.out($af['title']).'<br />';
-											}
-											echo '</p>';
-										}
-										echo '</td>
-								</tr>
-							</tbody>
-						</table>
-					</div>
+			echo'
+			<h1>Admin pagina van '.$row['name'].'</h1>
+			<p>U kunt hier zien welke evenementen door '.$row['name'].' aangemaakt/goedgekeurd/afgekeurd zijn.<br />
+				Ook kunt u diens rechten hier aanpassen.
+			</p>
+			<p>De volgende evenementen zijn door '.$row['name'].' aangemaakt:</p>
+				
+			<div class="user_events">
+				<table id="user_events">
+					<tbody>
+						<tr>
+							<th>Ongekeurde Evenementen</th>
+							<th>Goedgekeurde Evenementen</th>
+							<th>Afgekeurde Evenementen</th>
+						</tr>
+						<tr>
+							<td class="admin_td">';
+								$sql_ongekeurd = "SELECT title,id FROM events_status WHERE create_id=".$_GET['id']." AND status='unapproved'";
+								$stmt_ongekeurd = $mysqli->prepare($sql_ongekeurd);
+								$stmt_ongekeurd->execute();
+								
+								if($stmt_ongekeurd->rowCount() == 0)
+								{
+									echo '<p>Geen events</p>';
+								}
+								else
+								{
+									echo '<p>';
+									while($on = $stmt_ongekeurd->fetch())
+									{
+										echo '<a href="index.php?page=evenement&amp;id='.$on['id'].'&amp;semipage=keuren">'.out($on['title']).'</a><br />';
+									}
+									echo '</p>';
+								}
+							echo '</td>
+							<td class="admin_td">';
+								$sql_goedgekeurd = "SELECT title,id FROM events_status WHERE create_id=".$_GET['id']." AND status='approved'";
+								$stmt_goedgekeurd = $mysqli->prepare($sql_goedgekeurd);
+								$stmt_goedgekeurd->execute();
+								
+								if($stmt_goedgekeurd->rowCount() == 0)
+								{
+									echo '<p>Geen events</p>';
+								}
+								else
+								{
+									echo '<p>';
+									while($goed = $stmt_goedgekeurd->fetch())
+									{
+										echo '<a href="index.php?page=evenement&amp;id='.$goed['id'].'&amp;semipage=agenda_week">'.out($goed['title']).'</a><br />';
+									}
+									echo '</p>';
+								}
+							echo '</td>
+							<td class="admin_td">';
+								$sql_afgekeurd = "SELECT title,id FROM events_status WHERE create_id=".$_GET['id']." AND status='declined'";
+								$stmt_afgekeurd = $mysqli->prepare($sql_afgekeurd);
+								$stmt_afgekeurd->execute();
+								
+								if($stmt_afgekeurd->rowCount() == 0)
+								{
+									echo '<p>Geen events</p>';
+								}
+								else
+								{	
+									echo '<p>';
+									while($af = $stmt_afgekeurd->fetch())
+									{
+										echo ''.out($af['title']).'<br />';
+									}
+									echo '</p>';
+								}
+							echo '</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 
-					<p>Op dit moment heeft '.$row['name'].' de volgende rechten:</p>
-					<form id="admin" action="index.php?page=admin&amp;semipage=lijst_van_gebruikers&amp;id='.$_GET['id'].'" method="post">
-					
-					<input type="checkbox" name="admin_check[]" value="submit_event" id="submit_event" />Evenementen aanmaken
-					<input type="checkbox" name="admin_check[]" value="approve_event" id="approve_event" />Evenementen keuren
-					<input type="checkbox" name="admin_check[]" value="admin_rights" id="admin_rights" />Admin rechten<br />';
-					
-					echo' <span class="submit_button">
-								<button class="button" type="submit">
-									<span class="right"><span class="inner">Pas rechten aan</span></span>
-								</button>
-						  </span>
-					</form>';
+			<p>Op dit moment heeft '.$row['name'].' de volgende rechten:</p>
+			<form id="admin" action="index.php?page=admin&amp;semipage=lijst_van_gebruikers&amp;id='.$_GET['id'].'" method="post">
+				<input type="checkbox" name="admin_check[]" value="submit_event" id="submit_event" />Evenementen aanmaken
+				<input type="checkbox" name="admin_check[]" value="approve_event" id="approve_event" />Evenementen keuren
+				<input type="checkbox" name="admin_check[]" value="admin_rights" id="admin_rights" />Admin rechten<br />';
+			
+				echo' <span class="submit_button">
+					<button class="button" type="submit">
+						<span class="right"><span class="inner">Pas rechten aan</span></span>
+					</button>
+				</span>
+			</form>';
 		}
 	}
 	catch(Exception $exception)
