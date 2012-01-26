7db0eec071031a10dfc5961206b34b7dc7108443
Ivan Plantevin <ivan.p92@gmail.com>
2012-01-26 15:59:30 +0100

    Id ipv ID

--- htdocs/showEvents.js
+++ htdocs/showEvents.js
@@ -49,7 +49,7 @@ function updateShownEvents()
 function init(week)
 {
 	var checkboxes = document.getElementsByClassName("catbox");
-	var weekbox = document.getElementsByID("week_box");
+	var weekbox = document.getElementsById("week_box");
 	weekbox.value = week;
 	var checked = new Array();
 	
