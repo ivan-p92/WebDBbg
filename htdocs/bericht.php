c6d7abee3d63ca4ed99e8fc457c7225977d7ebe4
David Woudenberg <david_woudenberg@hotmail.com>
2012-01-26 14:42:44 +0100

    message pagina toegevoegd en lijst van berichten op admin pagina

--- htdocs/bericht.php
+++ htdocs/bericht.php
@@ -22,5 +22,43 @@ if($stmt->rowCount() == 1)
 {		
 	$row = $stmt->fetch();
 }
+else 
+{
+	echo' <script>alert(\''."Er ging iets mis met het laden van het bericht.\nVermoedlijk is het al verwijderd".'\')</script>
+	<meta http-equiv="refresh" content="0; url=http://websec.science.uva.nl/webdb1235/index.php?page=lijst_van_gebruikers" />';
+}
+
+echo'
+<table id="message">
+	<tbody>
+		<tr>
+			<td>
+				Bericht geplaatst door
+			</td>
+			<td>
+				'.out($row["name"]).'
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Email
+			</td>
+			<td>
+				'.out(row["email"]).'
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Bericht
+			</td>
+			<td>
+				'.out($row["message"]).'
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+
+
 
 ?>
