59d7bd75cf9e18d1d4a27257a605dffcbff9ebaf
freek boutkan <freek.boutkan@gmail.com>
2012-01-27 13:59:06 +0100

    bugfix admin

--- htdocs/header.php
+++ htdocs/header.php
@@ -7,7 +7,12 @@
 	<link rel="stylesheet" type="text/css" href="css.css" title="CSS" />
 	<link rel="shortcut icon" type="image/x-ico" href="afbeeldingen/favicon.ico" />
 	<script type="text/javascript" src="jquery.js"></script>
-	<script type="text/javascript" src="liveSearch.js"></script>
+	<?php
+	if(PAGE == 'lijst_van_gebruikers')
+	{
+		echo '<script type="text/javascript" src="liveSearch.js"></script>';
+	}
+	?>
 	<script type="text/javascript" src="showEvents.js"></script>
 </head>
 
