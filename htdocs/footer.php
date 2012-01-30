7de392083df2c1f335432187d506378761df9522
Vincent Velthuis <v.velthuis@gmail.com>
2012-01-30 13:23:37 +0100

    footer comments

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -1,13 +1,16 @@
-<div class="clearer">&nbsp;</div>
-</div>
-<div id="namen">
-<div id="namen_text">
-&copy; 2012 Freek Boutkan,
-Ivan Plantevin,
-Vincent Velthuis &amp;
-David Woudenberg
-</div>
-</div>
-</div>
+<!--Dit is de footer van de webpagina
+.clearer zorgt voor de juiste plaatsing van .namen-->
+		<div class="clearer">&nbsp;</div>
+	</div><!--Sluiter van .content uit header.php-->
+	<div id="namen">
+		<!--Namen van Webdb1235 in alfabetische volgorde-->
+		<div id="namen_text">
+		&copy; 2012 Freek Boutkan,
+		Ivan Plantevin,
+		Vincent Velthuis &amp;
+		David Woudenberg
+		</div>
+	</div>
+</div><!--Sluiter van .maincontainer uit header.php-->
 </body>
 </html>
